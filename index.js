36d8890387fe778fafb5789118fa63305c7f508d
Triet Nguyen <140176053+Alrmendo@users.noreply.github.com>
2025-02-17 14:47:40 +0700

    v2.0-db connect, helper for ES

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-const express = require('express');
-const path = require('path');
-const studentRoutes = require('./routes/studentRoutes');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { engine } from 'express-handlebars';
+import studentRoutes from './routes/studentRoutes.js';
+import databaseService from './database/db.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = 3000;
 
-// Cấu hình Handlebars
+// Kết nối MongoDB
+databaseService.connectDatabase();
+
+// Cấu hình Handlebars với helper `json`
+app.engine('hbs', engine({
+    extname: '.hbs',
+    layoutsDir: path.join(__dirname, 'views'), 
+    defaultLayout: 'layout',
+    partialsDir: path.join(__dirname, 'views/partials'),
+    helpers: {
+        json: function (context) {
+            return JSON.stringify(context);
+        }
+    },
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+        allowProtoMethodsByDefault: true
+    }
+}));
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -13,9 +39,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Cấu hình public folder (chứa CSS, JS, Images)
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Định tuyến
 app.use('/', studentRoutes);
 
+// Chạy server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
