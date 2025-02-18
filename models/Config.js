512cf0db6372895cc6735a6083db36e00f385ef7
Triet Nguyen <140176053+Alrmendo@users.noreply.github.com>
2025-02-18 12:02:15 +0700

    v2.0-Add congig model for edit department, status, program

---
+++ models/Config.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const configSchema = new mongoose.Schema({
+    departments: { type: [String], default: ["Khoa Luật", "Khoa Tiếng Anh thương mại", "Khoa Tiếng Nhật", "Khoa Tiếng Pháp"] },
+    statuses: { type: [String], default: ["Đang học", "Đã tốt nghiệp", "Đã thôi học", "Tạm dừng học"] },
+    programs: { type: [String], default: ["Chất lượng cao", "Đại trà"] }
+});
+
+const Config = mongoose.model('Config', configSchema);
+export default Config;
